c432e3c9d5b97ad13660bf35a647c55e2c73c91a
Daniel <dbgodfrey89@msn.com>
2018-08-11 16:58:29 -0600

    Switch out previous bmc.py and add Color.py

---
+++ src/Color.py
@@ -0,0 +1,17 @@
+import abc
+
+
+class Color(abc.ABC):
+  @abc.abstractmethod
+  def rgb(self):
+    pass
+
+
+class White(abc.ABC):
+  def rgb(self):
+    return (255, 255, 255)
+
+
+class Black(abc.ABC):
+  def rgb(self):
+    return (0, 0, 0)
