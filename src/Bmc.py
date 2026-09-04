9d97c08eb44c6d432369212fe0432162d89768ef
Daniel Godfrey <dbgodfrey89@msn.com>
2026-09-04 15:20:30 -0600

    :sparkles: Add game timer
    User can see how much time they're taking and it updates the score. There are probably unnecessary redraws :poop:, but it doesn't seem to be affecting performance so far. An extracted function :recycle: for redrawing score may indicate a way to move more code out of Bmc main file. That (and testing) should probably done before conclusion of this issue.
    
    Upped the ante a little with scores. It now takes only 20 seconds or 4 hints to zero out a correct guess.

--- src/Bmc.py
+++ src/Bmc.py
@@ -33,13 +33,21 @@ width = n_cols * avg_char_width
 height = n_rows * avg_char_height
 screen = Display.Screen.size_and_color(width=width, height=height, color=background)
 
+# Game timer
+clock = pygame.time.Clock()
+TIMER_EVENT = pygame.USEREVENT + 1 # Define a unique custom event ID
+# Set the timer to trigger every second 
+MS_PER_SECOND = 1000
+pygame.time.set_timer(TIMER_EVENT, millis=MS_PER_SECOND) 
+
 max_chars = 60
 wrapper = textwrap.TextWrapper(max_chars)
 
 correct = Score.Correct()
 incorrect = Score.Incorrect()
 hints = Score.Hints()
-total = Score.Total.Standard(correct, incorrect, hints)
+timer = Score.Timer()
+total = Score.Total.Standard(correct, incorrect, hints, timer)
 
 
 def next_start_x(textbox, *, x_offset):
@@ -52,13 +60,12 @@ def next_start_x(textbox, *, x_offset):
   x_rect_end = x_rect_start + rect_width 
   return x_rect_end + x_pad
 
-def redraw(lines, screen):
+def redraw_tallies(screen):
   global correct 
   global incorrect
   global hints
+  global timer
   global total
-
-  # SCORE
 
   correct_text = f'Correct: {correct.count():05d}'
   correct_start = 0
@@ -72,17 +79,28 @@ def redraw(lines, screen):
   hint_start = next_start_x(incorrect_textbox, x_offset=incorrect_start)
   hint_textbox = Display.TextBox.default_modified(hint_text, font, text_color, hint_start, 0)
 
-  score_text = f'Score: {total.value():07d}'
-  score_start = next_start_x(hint_textbox, x_offset=hint_start)
-  score_textbox = Display.TextBox.default_modified(score_text, font, text_color, score_start, 0)
+  timer_text = f'Timer: {timer.count():05d}'
+  timer_start = next_start_x(hint_textbox, x_offset=hint_start)
+  timer_textbox = Display.TextBox.default_modified(timer_text, font, text_color, timer_start, 0)
 
+  score_text = f'Score: {total.value():07d}'
+  score_start = next_start_x(timer_textbox, x_offset=timer_start)
+  score_textbox = Display.TextBox.default_modified(score_text, font, text_color, score_start, 0)
 
   displays = []
   displays.append(correct_textbox)
   displays.append(incorrect_textbox)
   displays.append(hint_textbox)
+  displays.append(timer_textbox)
   displays.append(score_textbox)
 
+  screen.blit(displays)
+
+  return displays
+
+
+def redraw(lines, screen):
+  displays = redraw_tallies(screen)
   for i, line in enumerate(lines):
     x = 0
     y = i * font_size
@@ -150,6 +168,11 @@ for verse in verses:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         sys.exit()
+
+      # Timer
+      if event.type == TIMER_EVENT:
+        timer.increment()
+        redraw(lines, screen)
 
       if event.type == pygame.KEYDOWN:
         pressed_keys = pygame.key.get_pressed()
--- src/Score.py
+++ src/Score.py
@@ -35,6 +35,13 @@ class Hints(Tally):
     super().__init__()
 
 
+class Timer(Tally):
+  '''Keeps track of how many seconds have passed.'''
+
+  def __init__(self):
+    super().__init__()
+
+
 class Score:
   '''Calculates the score of a tally.'''
 
@@ -50,19 +57,22 @@ class Total:
   '''Calculates the total score.'''
 
   @classmethod
-  def Standard(cls, correct, incorrect, hints):
+  def Standard(cls, correct, incorrect, hints, timer):
     correct_score = Score(correct, multiplier=100)
     incorrect_score = Score(incorrect, multiplier=50)
-    hints_score = Score(hints, multiplier=10)
-    return cls(correct_score, incorrect_score, hints_score)
+    hints_score = Score(hints, multiplier=25)
+    timer_score = Score(timer, multiplier=5)
+    return cls(correct_score, incorrect_score, hints_score, timer_score)
 
-  def __init__(self, correct, incorrect, hints):
+  def __init__(self, correct, incorrect, hints, timer):
     self._correct = correct
     self._incorrect = incorrect
     self._hints = hints
+    self._timer = timer
 
   def value(self):
     correct = self._correct.value()
     incorrect = self._incorrect.value()
     hints  = self._hints.value()
-    return correct - incorrect - hints
+    timer = self._timer.value()
+    return correct - incorrect - hints - timer
